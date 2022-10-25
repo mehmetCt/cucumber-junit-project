0d7ce1c915f959d9d01b627590a2cad682527783
adamar <aysun114@hotmail.com>
2022-10-25 13:56:16 -0500

    GoogleSearchPage added

---
+++ src/test/java/com/cydeo/pages/GoogleSearchPage.java
@@ -0,0 +1,17 @@
+package com.cydeo.pages;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class GoogleSearchPage {
+
+    public GoogleSearchPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(name="q")
+    public WebElement searchBox;
+
+}
--- src/test/java/com/cydeo/step_definitions/GoogleSearch_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/GoogleSearch_StepDefinitions.java
@@ -8,17 +8,16 @@ public class GoogleSearch_StepDefinitions {
 
     @Given("user is on Google search page")
     public void user_is_on_google_search_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
+
     @When("user types apple in the google search box and clicks enter")
     public void user_types_apple_in_the_google_search_box_and_clicks_enter() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
+
     @Then("user sees apple – Google Search is in the google title")
     public void user_sees_apple_google_search_is_in_the_google_title() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 }
