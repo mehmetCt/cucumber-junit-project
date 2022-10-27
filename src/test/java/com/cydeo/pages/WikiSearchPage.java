4e4cde01670ab4c9cff93ef4913cc46ad5fbb250
adamar <aysun114@hotmail.com>
2022-10-27 10:39:00 -0500

    WikiSearchPage and WikiSearch_StepDef added

--- src/test/java/com/cydeo/pages/WikiSearchPage.java
+++ src/test/java/com/cydeo/pages/WikiSearchPage.java
@@ -1,6 +1,9 @@
 package com.cydeo.pages;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class WikiSearchPage {
@@ -8,6 +11,15 @@ public class WikiSearchPage {
     public WikiSearchPage(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
+
+    @FindBy(id="searchInput")
+    public WebElement searchBox;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    public WebElement searchButton;
+
+
+
 
 
 }
