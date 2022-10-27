31dedba0ccf819b2e66217cc366c1d74c7804c0f
adamar <aysun114@hotmail.com>
2022-10-27 10:48:08 -0500

    WikiSearchPage and WikiSearch_StepDef added

--- src/test/java/com/cydeo/step_definitions/WikiSearch_StepDef.java
+++ src/test/java/com/cydeo/step_definitions/WikiSearch_StepDef.java
@@ -1,29 +1,35 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.pages.WikiSearchPage;
+import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class WikiSearch_StepDef {
 
+    WikiSearchPage wikiSearchPage = new WikiSearchPage();
 
     @Given("User is on Wikipedia home page")
     public void user_is_on_wikipedia_home_page() {
-
+        Driver.getDriver().get("https://www.wikipedia.org/");
     }
 
     @When("User types Steve Jobs in the wiki search box")
     public void user_types_steve_jobs_in_the_wiki_search_box() {
-
+      wikiSearchPage.searchBox.sendKeys("Steve Jobs");
     }
 
     @When("User clicks wiki search button")
     public void user_clicks_wiki_search_button() {
-
+      wikiSearchPage.searchButton.click();
     }
 
     @Then("User sees Steve Jobs is in the wiki title")
     public void user_sees_steve_jobs_is_in_the_wiki_title() {
+
+        Assert.assertTrue("Title verification is failed!",Driver.getDriver().getTitle().contains("Steve Jobs"));
 
     }
 }
