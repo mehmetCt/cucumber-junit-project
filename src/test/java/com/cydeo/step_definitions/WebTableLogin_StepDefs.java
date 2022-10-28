ee3ed708b7e7bf7ec31c7124ea9355e35e12ba3d
adamar <aysun114@hotmail.com>
2022-10-28 14:50:43 -0500

    WebTableLogin_StepDefs added

--- src/test/java/com/cydeo/step_definitions/WebTableLogin_StepDefs.java
+++ src/test/java/com/cydeo/step_definitions/WebTableLogin_StepDefs.java
@@ -1,33 +1,39 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.pages.WebTableLoginPage;
+import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class WebTableLogin_StepDefs {
 
+    WebTableLoginPage webTableLoginPage = new WebTableLoginPage();
+
     @Given("user is on the login page of web table app")
     public void user_is_on_the_login_page_of_web_table_app() {
-
+        Driver.getDriver().get(ConfigurationReader.getProperty("env"));
     }
 
     @When("user enters username {string}")
     public void user_enters_username(String string) {
-
+        webTableLoginPage.inputUsername.sendKeys(string);
     }
 
     @When("user enters password {string}")
     public void user_enters_password(String string) {
-
+        webTableLoginPage.inputPassword.sendKeys(string);
     }
 
     @When("user clicks to login button")
     public void user_clicks_to_login_button() {
-
+       webTableLoginPage.loginButton.click();
     }
 
     @Then("user should see url contains orders")
     public void user_should_see_url_contains_orders() {
-
+        Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains("orders"));
     }
 }
