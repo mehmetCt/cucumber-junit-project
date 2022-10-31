a94ab1ddee13a28d32b074b8907fcab2e6d8f4c3
adamar <aysun114@hotmail.com>
2022-10-31 15:30:42 -0500

    FailedTestRunner class added

--- src/test/java/com/cydeo/runner/CukesRunner.java
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features" ,
         glue = "com/cydeo/step_definitions" ,
         dryRun = false,
-        tags = "@maleScientist"
+        tags = "@femaleScientist"
 )
 public class CukesRunner {
 
