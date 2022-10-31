755f336793eb71ea6e6edc513fa39827105dc177
adamar <aysun114@hotmail.com>
2022-10-31 09:51:07 -0500

    BasePage class added

---
+++ src/test/java/com/cydeo/pages/BasePage.java
@@ -0,0 +1,26 @@
+package com.cydeo.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class BasePage {
+
+    @FindBy(xpath = "//button[@class=\"button nav__item active\"]")
+    public WebElement viewAllOrdersButtonNav;
+
+    @FindBy(css = "a[href='/products'] button")
+    public WebElement viewAllProductsButtonNav;
+
+    @FindBy(xpath = "//*[text() = 'Order']")
+    public WebElement orderButtonNavItem;
+
+    @FindBy(xpath = "//button[@class=\"button __logout\"]")
+    public WebElement logoutButton;
+
+    public BasePage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+}
