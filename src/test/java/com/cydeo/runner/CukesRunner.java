0695536c74ecee58620458f996b04eede90257c9
adamar <aysun114@hotmail.com>
2022-11-01 10:27:43 -0500

    CukesRunner class added

--- src/test/java/com/cydeo/runner/CukesRunner.java
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -8,7 +8,8 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {
                 "html:target/cucumber-report.html",
-                "rerun:target/rerun.txt"
+                "rerun:target/rerun.txt" ,
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
         },
         features = "src/test/resources/features" ,
         glue = "com/cydeo/step_definitions" ,
--- src/test/java/com/cydeo/runner/FailedTestRunner.java
+++ src/test/java/com/cydeo/runner/FailedTestRunner.java
@@ -11,3 +11,5 @@ import org.junit.runner.RunWith;
 )
 public class FailedTestRunner {
 }
+
+// This runner class will be running only failed scenarios
